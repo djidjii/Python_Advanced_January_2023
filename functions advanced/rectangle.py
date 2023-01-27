54ae2f939c454e1641b65a0943b2fd9a1de93fd1
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-01-27 20:26:24 +0200

    Add files via upload

---
+++ functions advanced/rectangle.py
@@ -0,0 +1,12 @@
+def rectangle(length, width):
+    def rectangle_area():
+        return length * width
+
+    def rectangle_perimeter():
+        return 2 * (length + width)
+
+    if type(length) != int or type(width) != int:
+        return "Enter valid values!"
+
+    return f'''Rectangle area: {rectangle_area()}
+Rectangle perimeter: {rectangle_perimeter()}'''
