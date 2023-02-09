5c177942855d12c1d3bc4eecf729a2365f8bff49
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-02-09 19:09:50 +0200

    Update workshop.py

--- simple game/workshop.py
+++ simple game/workshop.py
@@ -7,7 +7,7 @@ def create_matrix(rows: int, columns: int):
 
 
 def player_choice(player):
-    choice = int(input(f"Player{player} please choose a column:\n"))
+    choice = int(input(f'Player {player}, please choose a column:\n'))
 
     return choice - 1
 
@@ -26,68 +26,71 @@ def implement_player_data_to_main_logic(playground, player_index, player):
 def check_win_condition(playground, row_index, column_index, win_counter):
 
     def check_right_win_condition(playground, row_index, column_index, max_column_index):
-        right_value = [playground[row_index][1] for i in range(column_index, max_column_index)]
-        return right_value
+        right_values = [playground[row_index][i] for i in range(column_index, max_column_index)]
+        return right_values
 
     def check_left_win_condition(playground, row_index, column_index, min_column_index):
-        left_value = [playground[row_index][1] for i in range(column_index, min_column_index, -1)]
-        return left_value
+        left_values = [playground[row_index][i] for i in range(column_index, min_column_index, -1)]
+        return left_values
 
     def check_up_win_condition(playground, row_index, column_index, min_row_index):
-        up_value = [playground[i][column_index] for i in range(row_index, min_row_index, -1)]
-        return up_value
+        up_values = [playground[i][column_index] for i in range(row_index, min_row_index, -1)]
+        return up_values
 
     def check_down_win_condition(playground, row_index, column_index, max_row_index):
-        down_value = [playground[i][column_index] for i in range(row_index, max_row_index)]
-        return down_value
+        down_values = [playground[i][column_index] for i in range(row_index, max_row_index)]
+        return down_values
 
     def check_down_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index):
         diagonal_index = min(max_row_index - row_index, max_column_index - column_index)
-        down_right_value = [playground[row_index + i][column_index + i] for i in range(diagonal_index)]
-        return down_right_value
+        down_right_values = [playground[row_index + i][column_index + i] for i in range(diagonal_index)]
+        return down_right_values
 
-    def check_down_left_win_condition(playground, row_index, column_index, max_row_index, max_column_index):
+    def check_down_left_win_condition(playground, row_index, column_index, max_row_index, min_column_index):
         diagonal_index = min(max_row_index - row_index, abs(min_column_index - column_index))
-        down_left_value = [playground[row_index + i][column_index - i] for i in range(diagonal_index)]
-        return down_left_value
+        down_left_values = [playground[row_index + i][column_index - i] for i in range(diagonal_index)]
+        return down_left_values
 
-    def check_up_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index):
+    def check_up_right_win_condition(playground, row_index, column_index, min_row_index, max_column_index):
         diagonal_index = min(abs(min_row_index - row_index), max_column_index - column_index)
-        up_right_value = [playground[row_index - i][column_index + i] for i in range(diagonal_index)]
-        return up_right_value
+        up_right_values = [playground[row_index - i][column_index + i] for i in range(diagonal_index)]
+        return up_right_values
 
     def check_up_left_win_condition(playground, row_index, column_index, min_row_index, min_column_index):
-        diagonal_index = min(abs(min_row_index - row_index), min_column_index - column_index)
-        up_left_value = [playground[row_index - i][column_index - i] for i in range(diagonal_index)]
-        return up_left_value
+        diagonal_index = min(abs(min_row_index - row_index), abs(min_column_index - column_index))
+        up_left_values = [playground[row_index - i][column_index - i] for i in range(diagonal_index)]
+        return up_left_values
 
+    # indexes
     max_column_index = min(column_index + win_counter, len(playground[column_index]))
     min_column_index = max(-1, column_index - win_counter)
     max_row_index = min(row_index + win_counter, len(playground))
     min_row_index = max(-1, row_index - win_counter)
 
-    right_win_condition_value = check_right_win_condition(playground, row_index, column_index, max_column_index)
-    left_win_condition_value = check_left_win_condition(playground, row_index, column_index, min_column_index)
-    up_win_condition_value = check_up_win_condition(playground, row_index, column_index, min_row_index)
-    down_win_condition_value = check_down_win_condition(playground, row_index, column_index, max_row_index)
+    # left, right, up, down
+    right_win_condition_values = check_right_win_condition(playground, row_index, column_index, max_column_index)
+    left_win_condition_values = check_left_win_condition(playground, row_index, column_index, min_column_index)
+    up_win_condition_values = check_up_win_condition(playground, row_index, column_index, min_row_index)
+    down_win_condition_values = check_down_win_condition(playground, row_index, column_index, max_row_index)
 
-    down_right_win_condition_value = check_down_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index)
-    down_left_win_condition_value = check_down_left_win_condition(playground, row_index, column_index, max_row_index, min_column_index)
-    up_right_win_condition_value = check_up_right_win_condition(playground, row_index, column_index, min_row_index, max_column_index)
-    up_left_win_condition_value = check_up_left_win_condition(playground, row_index, column_index, min_row_index, min_column_index)
+    # diagonals
+    down_right_win_condition_values = check_down_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index)
+    down_left_win_condition_values = check_down_left_win_condition(playground, row_index, column_index, max_row_index, min_column_index)
+    up_right_win_condition_values = check_up_right_win_condition(playground, row_index, column_index, min_row_index, max_column_index)
+    up_left_win_condition_values = check_up_left_win_condition(playground, row_index, column_index, min_row_index, min_column_index)
 
     possible_winner_checker = [
-        right_win_condition_value,
-        left_win_condition_value,
-        up_win_condition_value,
-        down_win_condition_value,
-        down_right_win_condition_value,
-        down_left_win_condition_value,
-        up_right_win_condition_value,
-        up_left_win_condition_value,
+        right_win_condition_values,
+        left_win_condition_values,
+        up_win_condition_values,
+        down_win_condition_values,
+        down_right_win_condition_values,
+        down_left_win_condition_values,
+        up_right_win_condition_values,
+        up_left_win_condition_values,
     ]
 
-    return any((current_list) == win_counter and len(set(current_list)) == 1 for current_list in possible_winner_checker)
+    return any(len(current_list) == win_counter and len(set(current_list)) == 1 for current_list in possible_winner_checker)
 
 
 def game_play_function(playground, win_counter):
@@ -99,7 +102,7 @@ def game_play_function(playground, win_counter):
         print_function(playground)
 
         if check_win_condition(playground, row_index, column_index, win_counter):
-            print(f"The winner is player {current_player}")
+            print(f'The winner is player {current_player}')
             break
 
         current_player, second_player = second_player, current_player
@@ -117,7 +120,4 @@ try:
     playground = create_matrix(rows, columns)
     game_play_function(playground, win_counter)
 except ValueError:
-    print("Invalid values, try again!")
-
-
-playground = create_matrix(6, 7)
+    print('Invalid values, try again!')
