5b97ed78c679029252ed291ce339c334bf6f69c2
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-02-10 19:59:44 +0200

    Update workshop.py

--- simple game/workshop.py
+++ simple game/workshop.py
@@ -23,79 +23,48 @@ def implement_player_data_to_main_logic(playground, player_index, player):
     return start_row_index - 1, player_index
 
 
-def check_win_condition(playground, row_index, column_index, win_counter):
+def is_win_condition(player, player_row, player_column, playground, win_counter):
 
-    def check_right_win_condition(playground, row_index, column_index, max_column_index):
-        right_values = [playground[row_index][i] for i in range(column_index, max_column_index)]
-        return right_values
+    def convert_player_position_in_direction(playground, initial_row, initial_column, row_change, column_change):
+        row, column = initial_row, initial_column
+        row_change *= -1
+        column_change *= -1
 
-    def check_left_win_condition(playground, row_index, column_index, min_column_index):
-        left_values = [playground[row_index][i] for i in range(column_index, min_column_index, -1)]
-        return left_values
+        while 0 <= row < len(playground) and 0 <= column < len(playground[0]):
+            if playground[row][column] != player:
+                break
+            column += column_change
+            row += row_change
 
-    def check_up_win_condition(playground, row_index, column_index, min_row_index):
-        up_values = [playground[i][column_index] for i in range(row_index, min_row_index, -1)]
-        return up_values
+        if row == initial_row and column == initial_column:
+            return row, column
 
-    def check_down_win_condition(playground, row_index, column_index, max_row_index):
-        down_values = [playground[i][column_index] for i in range(row_index, max_row_index)]
-        return down_values
+        return row - row_change, column - column_change
 
-    def check_down_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index):
-        diagonal_index = min(max_row_index - row_index, max_column_index - column_index)
-        down_right_values = [playground[row_index + i][column_index + i] for i in range(diagonal_index)]
-        return down_right_values
+    def is_win_condition_in_direction(playground, initial_row, initial_column, direction, win_counter):
+        row_change, column_change = direction
+        row, column = convert_player_position_in_direction(playground, initial_row, initial_column, row_change, column_change)
+        rows_border = min(len(playground), row + win_counter * row_change)
+        columns_border = min(len(playground[0]), column + win_counter * column_change)
+        row_included_condition = rows_border == row
 
-    def check_down_left_win_condition(playground, row_index, column_index, max_row_index, min_column_index):
-        diagonal_index = min(max_row_index - row_index, abs(min_column_index - column_index))
-        down_left_values = [playground[row_index + i][column_index - i] for i in range(diagonal_index)]
-        return down_left_values
+        counter = 0
 
-    def check_up_right_win_condition(playground, row_index, column_index, min_row_index, max_column_index):
-        diagonal_index = min(abs(min_row_index - row_index), max_column_index - column_index)
-        up_right_values = [playground[row_index - i][column_index + i] for i in range(diagonal_index)]
-        return up_right_values
+        while (row != rows_border or row_included_condition) and (column != columns_border or (columns_border == column)) \
+                and player == playground[row][column]:
+            counter += 1
+            row += row_change
+            column += column_change
 
-    def check_up_left_win_condition(playground, row_index, column_index, min_row_index, min_column_index):
-        diagonal_index = min(abs(min_row_index - row_index), abs(min_column_index - column_index))
-        up_left_values = [playground[row_index - i][column_index - i] for i in range(diagonal_index)]
-        return up_left_values
+        return counter == win_counter
 
-    # indexes
-    max_column_index = min(column_index + win_counter, len(playground[column_index]))
-    min_column_index = max(-1, column_index - win_counter)
-    max_row_index = min(row_index + win_counter, len(playground))
-    min_row_index = max(-1, row_index - win_counter)
+    directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]
 
-    # left, right, up, down
-    right_win_condition_values = check_right_win_condition(playground, row_index, column_index, max_column_index)
-    left_win_condition_values = check_left_win_condition(playground, row_index, column_index, min_column_index)
-    up_win_condition_values = check_up_win_condition(playground, row_index, column_index, min_row_index)
-    down_win_condition_values = check_down_win_condition(playground, row_index, column_index, max_row_index)
-
-    # diagonals
-    down_right_win_condition_values = check_down_right_win_condition(playground, row_index, column_index, max_row_index, max_column_index)
-    down_left_win_condition_values = check_down_left_win_condition(playground, row_index, column_index, max_row_index, min_column_index)
-    up_right_win_condition_values = check_up_right_win_condition(playground, row_index, column_index, min_row_index, max_column_index)
-    up_left_win_condition_values = check_up_left_win_condition(playground, row_index, column_index, min_row_index, min_column_index)
-
-    possible_winner_checker = [
-        right_win_condition_values,
-        left_win_condition_values,
-        up_win_condition_values,
-        down_win_condition_values,
-        down_right_win_condition_values,
-        down_left_win_condition_values,
-        up_right_win_condition_values,
-        up_left_win_condition_values,
-    ]
-
-    return any(len(current_list) == win_counter and len(set(current_list)) == 1 for current_list in possible_winner_checker)
-
-    # for current_list in possible_winner_checker:
-    #     if len(current_list) == win_counter and len(set(current_list)) == 1:
-    #         return True
-    # return False
+    for direction in directions:
+        if is_win_condition_in_direction(playground, player_row, player_column, direction, win_counter):
+            return True
+    else:
+        return False
 
 def game_play_function(playground, win_counter):
     current_player, second_player = 1, 2
@@ -105,11 +74,12 @@ def game_play_function(playground, win_counter):
         row_index, column_index = implement_player_data_to_main_logic(playground, current_player_choice, current_player)
         print_function(playground)
 
-        if check_win_condition(playground, row_index, column_index, win_counter):
+        if is_win_condition(current_player, row_index, column_index, playground, win_counter):
             print(f'The winner is player {current_player}')
             break
 
         current_player, second_player = second_player, current_player
+
 
 def print_function(playground):
     for row in playground:
